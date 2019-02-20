49950324865b07388313049c10e4da3130c40086
utsuki-protein <utsuki.xxx@gmail.com>
2019-02-20 22:31:23 +0900

    Add ListView Sample Fix Intent Data
    NextStep:Sqlite Access

--- app/src/main/java/com/example/utsuki/basketball_shooting/MainActivity.kt
+++ app/src/main/java/com/example/utsuki/basketball_shooting/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.utsuki.basketball_shooting
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,10 +15,17 @@ class MainActivity : AppCompatActivity() {
         val SaveButton: Button = findViewById(R.id.save)
         val TotalText: TextView = findViewById(R.id.Total)
         val CountText: TextView = findViewById(R.id.Count)
+
         SaveButton.setOnClickListener{
             var total: Int = Integer.valueOf(TotalText.text.toString())
             var count: Int = Integer.valueOf(CountText.text.toString())
-            Log.d("TEST",  count.toString() + "/" + total.toString())
+            val intent = Intent(this, ShootingHistory::class.java)
+            val shoot_str = "%4d/ %4d".format(count, total)
+            Log.d("MAIN",  shoot_str)
+            intent.putExtra("INSERT_DATA", shoot_str)
+            //画面遷移を開始
+            startActivity(intent)
         }
+
     }
 }
--- app/src/main/java/com/example/utsuki/basketball_shooting/ShootingHistory.kt
+++ app/src/main/java/com/example/utsuki/basketball_shooting/ShootingHistory.kt
@@ -2,11 +2,35 @@ package com.example.utsuki.basketball_shooting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import android.widget.ArrayAdapter
+
+
 
 class ShootingHistory : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shooting_history)
+        //ListViewのセット
+        val listView = ListView(this)
+        setContentView(listView)
+
+        //データの追加
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        adapter.add("10 / 20")
+        adapter.add("15 / 24")
+        adapter.add("1 / 3")
+        //受け取った変数を入れる
+        val text = intent.getStringExtra("INSERT_DATA")
+        Log.d("HIST", text)
+
+//        Sqlite3からデータを読み込み
+//        adapter.add(SQLiteRead)
+
+
+        listView.setAdapter(adapter);
     }
 }
